b2e599a9917c69b82bf34feb508fea95fb15d794
Ian Knott <ianmatknott@gmail.com>
2021-06-08 15:17:38 -0700

    commit 'adjusted settings.py file'

--- ians_webpage/settings.py
+++ ians_webpage/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My apps
+    'ians_py_page',
+
+    #Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
