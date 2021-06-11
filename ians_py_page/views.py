b3dd964b5b31f3b144348e178e6f9a7c9f82d213
Ian Knott <ianmatknott@gmail.com>
2021-06-10 17:16:59 -0700

    commit "randomchanges look at rd"

--- ians_py_page/views.py
+++ ians_py_page/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     """The homepage for ians_py_page"""
-    return render(request, 'ians_py_page/index.html')
+    return render(request, '/Users/ianknott/Desktop/ians_webpage/ians_py_page/templates/index.html')
